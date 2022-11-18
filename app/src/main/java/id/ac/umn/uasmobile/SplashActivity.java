f55ad7cd925b8499a05ccdce25200f1ae74b75e8
Felix <felixsutanto2712@gmail.com>
2022-11-18 12:03:38 +0700

    Create README.md

--- app/src/main/java/id/ac/umn/uasmobile/SplashActivity.java
+++ app/src/main/java/id/ac/umn/uasmobile/SplashActivity.java
@@ -1,23 +1,23 @@
 package id.ac.umn.uasmobile;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class SplashScreen extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_splash);
 
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intentSplashScreen = new Intent(SplashScreen.this, Register.class);
+                Intent intentSplashScreen = new Intent(SplashActivity.this, Register.class);
                 startActivity(intentSplashScreen);
                 finish();
             }
@@ -25,5 +25,4 @@ public class SplashScreen extends AppCompatActivity {
 
 
     }
-
 }
